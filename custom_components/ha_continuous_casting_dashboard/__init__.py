53aebd9bbdb91ec5681cf667690314f18c3ca90c
b0mbays <sam.s.lewis94@gmail.com>
2023-03-23 22:13:53 +0000

    Moving to a configuration.yaml file

--- custom_components/ha_continuous_casting_dashboard/__init__.py
+++ custom_components/ha_continuous_casting_dashboard/__init__.py
@@ -8,27 +8,15 @@ from homeassistant.core import HomeAssistant
 
 from homeassistant.const import CONF_DEVICES, CONF_SCAN_INTERVAL
 
-CONFIG_SCHEMA = vol.Schema(
-    {
-        DOMAIN: vol.Schema(
-            {
-                vol.Required(CONF_DEVICES): {cv.string: cv.url},
-                vol.Optional("cast_delay", default=10): int,
-            }
-        )
-    },
-    extra=vol.ALLOW_EXTRA,
-)
-
-async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+async def async_setup(hass: HomeAssistant, config: dict) -> bool:
     """Set up the Continuously Cast Dashboards integration."""
+    conf = config.get(DOMAIN)
+    if conf is None:
+        return True
+
     hass.data.setdefault(DOMAIN, {})
-    for platform in PLATFORMS:
-        hass.async_create_task(
-            hass.config_entries.async_forward_entry_setup(entry, platform)
-        )
 
     # Start the HaContinuousCastingDashboard
-    caster = HaContinuousCastingDashboard(hass, entry.data)
+    caster = HaContinuousCastingDashboard(hass, conf)
     hass.loop.create_task(caster.start())
     return True
